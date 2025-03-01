b0d5fc38f17a087789ccc55cf05e2393ac63c65d
Valentin <valentin@fintual.com>
2025-02-28 18:09:39 -0600

    Add gun angle util

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,124 @@
-/**
- * Some predefined delay values (in milliseconds).
- */
-export enum Delays {
-  Short = 500,
-  Medium = 2000,
-  Long = 5000,
+const targetPosition: Point = {
+  x: 0,
+  y: 1000,
+  z: 1000,
+};
+
+type Point = {
+  x: number;
+  y: number;
+  z: number;
+};
+
+type Triangle = {
+  opposite: number;
+  adjacent: number;
+  hypotenuse: number;
+};
+
+const gunPosition: Point = {
+  x: 3500,
+  y: 1000,
+  z: 6000,
+};
+
+const gunBaseTarget: Point = {
+  ...gunPosition,
+  z: 0,
+};
+
+type RadAngleValue = number;
+
+function getHorizontalTriangle(targetPosition: Point): Triangle {
+  return {
+    opposite: targetPosition.x - gunBaseTarget.x,
+    adjacent: gunPosition.z - targetPosition.z,
+    get hypotenuse() {
+      return Math.sqrt(this.opposite ** 2 + this.adjacent ** 2);
+    },
+  };
 }
 
-/**
- * Returns a Promise<string> that resolves after a given time.
- *
- * @param {string} name - A name.
- * @param {number=} [delay=Delays.Medium] - A number of milliseconds to delay resolution of the Promise.
- * @returns {Promise<string>}
- */
-function delayedHello(
-  name: string,
-  delay: number = Delays.Medium,
-): Promise<string> {
-  return new Promise((resolve: (value?: string) => void) =>
-    setTimeout(() => resolve(`Hello, ${name}`), delay),
+function getVerticalTriangle(
+  targetPosition: Point,
+  horizontalTriangle: Triangle,
+): Triangle {
+  return {
+    opposite: targetPosition.y - gunBaseTarget.y,
+    adjacent: horizontalTriangle.hypotenuse,
+    get hypotenuse() {
+      return Math.sqrt(this.opposite ** 2 + this.adjacent ** 2);
+    },
+  };
+}
+
+function getYaw(gunPosition: Point, targetPosition: Point): RadAngleValue {
+  const bottomWallTargetPosition = findBottomWallIntersection(
+    gunPosition,
+    targetPosition,
   );
+
+  const horizontalTriangle = getHorizontalTriangle(bottomWallTargetPosition);
+
+  const yaw = Math.atan(
+    horizontalTriangle.opposite / horizontalTriangle.adjacent,
+  );
+  return yaw;
 }
 
-// Please see the comment in the .eslintrc.json file about the suppressed rule!
-// Below is an example of how to use ESLint errors suppression. You can read more
-// at https://eslint.org/docs/latest/user-guide/configuring/rules#disabling-rules
+function getPitch(gunPosition: Point, targetPosition: Point): RadAngleValue {
+  const bottomWallTargetPosition = findBottomWallIntersection(
+    gunPosition,
+    targetPosition,
+  );
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-explicit-any
-export async function greeter(name: any) {
-  // The name parameter should be of type string. Any is used only to trigger the rule.
-  return await delayedHello(name, Delays.Long);
+  const horizontalTriangle = getHorizontalTriangle(bottomWallTargetPosition);
+
+  const verticalTriangle = getVerticalTriangle(
+    targetPosition,
+    horizontalTriangle,
+  );
+
+  const pitch = Math.atan(
+    verticalTriangle.opposite / verticalTriangle.adjacent,
+  );
+  return pitch;
 }
+
+function findBottomWallIntersection(
+  gunPosition: Point,
+  targetPosition: Point,
+): Point {
+  if (targetPosition.z === 0) {
+    return targetPosition;
+  } else {
+    return findSideWallIntersection(gunPosition, targetPosition);
+  }
+}
+
+function findSideWallIntersection(
+  gunPosition: Point,
+  targetPosition: Point,
+): Point {
+  const { x: xg, y: yg, z: zg } = gunPosition;
+  const { x: xt, y: yt, z: zt } = targetPosition;
+
+  if (xt === xg) {
+    throw new Error('Gun and target must not be aligned on x-axis.');
+  }
+
+  const t = -xg / (xt - xg);
+
+  return {
+    x: 0,
+    y: yg + t * (yt - yg),
+    z: zg + t * (zt - zg),
+  };
+}
+
+function radToDeg(rad: number): string {
+  return `${(rad * (180 / Math.PI)).toFixed(2)}°`;
+}
+
+console.log('yaw', radToDeg(getYaw(gunPosition, targetPosition)));
+console.log('pitch', radToDeg(getPitch(gunPosition, targetPosition)));
